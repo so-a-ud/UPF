e0aba51bcd387884ff1897b93a5c8ff477be3653
meremfg <meryemfg21@gmail.com>
2021-01-13 15:42:55 +0100

    projet

---
+++ CV_THEQUE/app/Models/Etudiant.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Etudiant extends Model
+{
+    protected $fillable = [
+        
+        'cin',
+        'cne',
+        'nom',
+        'prenom',
+        'dateDeNaissance',
+        'sexe',
+        'user_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
---
+++ CV_THEQUE/routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('admin/home', [App\Http\Controllers\HomeController::class, 'adminHome'])->name('admin.home')->middleware('is_admin');
+// Students
+Route::get('admin/students', [App\Http\Controllers\AdminController::class, 'students'])->name('admin.students')->middleware('is_admin');
+Route::get('admin/students/create', [App\Http\Controllers\AdminController::class, 'createStudent'])->name('admin.students.create')->middleware('is_admin');
+Route::post('admin/students/store', [App\Http\Controllers\AdminController::class, 'storeStudent'])->name('admin.students.store')->middleware('is_admin');
+Route::get('admin/students/edit/{cne}', [App\Http\Controllers\AdminController::class, 'editStudent'])->name('admin.students.edit')->middleware('is_admin');
+Route::post('admin/students/update', [App\Http\Controllers\AdminController::class, 'updateStudent'])->name('admin.students.update')->middleware('is_admin');
+Route::delete('admin/students/delete/{id}', [App\Http\Controllers\AdminController::class, 'destroyStudent'])->name('admin.students.destroy')->middleware('is_admin');
+
+// Recruteurs
+Route::get('admin/recruteurs', [App\Http\Controllers\AdminController::class, 'recruteurs'])->name('admin.recruteurs')->middleware('is_admin');
+Route::get('admin/recruteurs/create', [App\Http\Controllers\AdminController::class, 'createRec'])->name('admin.recruteurs.create')->middleware('is_admin');
+Route::post('admin/recruteurs/store', [App\Http\Controllers\AdminController::class, 'storeRec'])->name('admin.recruteurs.store')->middleware('is_admin');
+Route::get('admin/recruteurs/edit/{id}', [App\Http\Controllers\AdminController::class, 'editRec'])->name('admin.recruteurs.edit')->middleware('is_admin');
+Route::post('admin/recruteurs/update', [App\Http\Controllers\AdminController::class, 'updateRec'])->name('admin.recruteurs.update')->middleware('is_admin');
+Route::delete('admin/recruteurs/delete/{id}', [App\Http\Controllers\AdminController::class, 'destroyRec'])->name('admin.recruteurs.destroy')->middleware('is_admin');
+
